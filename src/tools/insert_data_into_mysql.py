046644dd94150f247bdf76eec42be992c6088bb2
dataharoon <170242230+haroondata@users.noreply.github.com>
2025-06-19 02:43:33 +0100

    changed insert statement to do a bulk insert

--- src/tools/insert_data_into_mysql.py
+++ src/tools/insert_data_into_mysql.py
@@ -9,7 +9,7 @@ from sqlalchemy import text
 
 
 #insert data
-def insert_data_into_mysql(mysql_connection,df):
+def insert_data_into_mysql(mysql_connection,df,table_name):
     """
     
 
@@ -25,21 +25,4 @@ def insert_data_into_mysql(mysql_connection,df):
     None.
 
     """    
-    for index, row in df.iterrows():
-        
-        invoice_number = row['invoice_number']
-        stock_code = row['stock_code']
-        description = row['description']
-        description =str(description)
-        description=description.replace("'","''")
-        quantity = row['quantity']
-        invoice_date = row['invoice_date']
-        unit_price= row['unit_price']
-        customer_id = row['customer_id']
-        country = row['country']
-        print(country)
-        sql_insert = """INSERT INTO retail_data (invoice_number	,stock_code,	description,	quantity,	invoice_date,unit_price,	customer_id,	country)
-        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}','{5}', '{6}', '{7}');""".format(invoice_number	,stock_code,	description,	quantity,	invoice_date,unit_price,	customer_id,	country)
-        sql_query_insert = text(sql_insert)
-        mysql_connection.execute(sql_query_insert) 
-    #connection.commit()
+    df.to_sql(table_name, con=mysql_connection, if_exists="append", index=False)
