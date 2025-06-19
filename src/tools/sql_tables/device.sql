cc3cfe8526cb6352e2d7fb26dcc54575002b8534
dataharoon <170242230+haroondata@users.noreply.github.com>
2025-06-19 02:42:38 +0100

    added sql create files

---
+++ src/tools/sql_tables/device.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `device` (
+  `resourcetype` VARCHAR(255) DEFAULT NULL,
+  `id` VARCHAR(255) DEFAULT NULL,
+  `meta_profile_0` VARCHAR(255) DEFAULT NULL,
+  `udicarrier` VARCHAR(255) DEFAULT NULL,
+  `status` VARCHAR(255) DEFAULT NULL,
+  `distinctidentifier` VARCHAR(255) DEFAULT NULL,
+  `manufacturedate` VARCHAR(255) DEFAULT NULL,
+  `expirationdate` VARCHAR(255) DEFAULT NULL,
+  `lotnumber` VARCHAR(255) DEFAULT NULL,
+  `serialnumber` VARCHAR(255) DEFAULT NULL,
+  `devicename` VARCHAR(255) DEFAULT NULL,
+  `type_coding` VARCHAR(255) DEFAULT NULL,
+  `type_text` VARCHAR(255) DEFAULT NULL,
+  `patient_reference` VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ src/tools/sql_tables/procedure.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `procedure` (
+  `resourcetype` VARCHAR(255) DEFAULT NULL,
+  `id` VARCHAR(255) DEFAULT NULL,
+  `meta_profile_0` VARCHAR(255) DEFAULT NULL,
+  `status` VARCHAR(255) DEFAULT NULL,
+  `code_coding` VARCHAR(255) DEFAULT NULL,
+  `code_text` VARCHAR(255) DEFAULT NULL,
+  `subject_reference` VARCHAR(255) DEFAULT NULL,
+  `encounter_reference` VARCHAR(255) DEFAULT NULL,
+  `performedperiod_start` VARCHAR(255) DEFAULT NULL,
+  `performedperiod_end` VARCHAR(255) DEFAULT NULL,
+  `location_reference` VARCHAR(255) DEFAULT NULL,
+  `location_display` VARCHAR(255) DEFAULT NULL,
+  `reasonreference` VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
