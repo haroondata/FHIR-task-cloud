b04a99ef01296688dc95bab5940b3f26a92ac4e8
dataharoon <170242230+haroondata@users.noreply.github.com>
2025-06-27 01:14:27 +0100

    added the files to the main.py file

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 # -*- coding: utf-8 -*-
 """
+Main entry point for the FHIR JSON to MySQL pipeline.
 Created on Sun Jun 23 17:44:26 2024
 
 @author: Haroon
 """
 
-from src.tools.read_json_data import get_username,get_data_from_json,write_sql_create_table_statements,create_csv_files_and_insert_data_into_mysql_db
+from src.tools.read_json_data import get_data_from_json
+from src.tools.create_sql_schema import write_sql_create_table_statements
+from src.tools.prepare_and_load_to_mysql import insert_data_into_mysql_db
+from src.tools import logger  # This runs the setup
+import logging
+from src.tools.mysql_connection import  mysql_connection
+mysql_engine,mysql_connection =  mysql_connection()
 
-
+def main():
+   try:
+       # Read and parse Json files into resource maps
+       resource_map, file_tracking_map = get_data_from_json()
+       logging.info(f"Extracted resources: {list(resource_map.keys())}")
+       
+       # Generate and execute SQL Table creation statements 
+       write_sql_create_table_statements(mysql_connection,resource_map)
+       
+       insert_data_into_mysql_db(mysql_engine,mysql_connection,resource_map, file_tracking_map)
+     
+   except Exception as e: 
+       logging.exception(f"Pipelines execution faile:{e}")      
+   
+   
 if __name__ == '__main__':
-
-
-   username = get_username()
-   resource_map = get_data_from_json(username)
-   write_sql_create_table_statements(resource_map)
-   create_csv_files_and_insert_data_into_mysql_db(resource_map)
-    
+    main()
