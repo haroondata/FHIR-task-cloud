ef0f54c4cdd0debef94781ce321dcf5ca8e7cc6d
dataharoon <170242230+haroondata@users.noreply.github.com>
2025-06-27 01:10:54 +0100

    sql files to create the schema

---
+++ sql_tables/allergyintolerance.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `allergyintolerance` (
+  `resourcetype` VARCHAR(255) DEFAULT NULL,
+  `id` VARCHAR(255) DEFAULT NULL,
+  `meta_profile_0` VARCHAR(255) DEFAULT NULL,
+  `clinicalstatus_coding` VARCHAR(255) DEFAULT NULL,
+  `verificationstatus_coding` VARCHAR(255) DEFAULT NULL,
+  `type` VARCHAR(255) DEFAULT NULL,
+  `category_0` VARCHAR(255) DEFAULT NULL,
+  `criticality` VARCHAR(255) DEFAULT NULL,
+  `code_coding` VARCHAR(255) DEFAULT NULL,
+  `code_text` VARCHAR(255) DEFAULT NULL,
+  `patient_reference` VARCHAR(255) DEFAULT NULL,
+  `recordeddate` VARCHAR(255) DEFAULT NULL,
+  `reaction` VARCHAR(255) DEFAULT NULL,
+  `patient_reference_uuid` VARCHAR(255) DEFAULT NULL,
+  `created_at` VARCHAR(255) DEFAULT NULL,
+  `updated_at` VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ sql_tables/immunization.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `immunization` (
+  `resourcetype` VARCHAR(255) DEFAULT NULL,
+  `id` VARCHAR(255) DEFAULT NULL,
+  `meta_profile_0` VARCHAR(255) DEFAULT NULL,
+  `status` VARCHAR(255) DEFAULT NULL,
+  `vaccinecode_coding` VARCHAR(255) DEFAULT NULL,
+  `vaccinecode_text` VARCHAR(255) DEFAULT NULL,
+  `patient_reference` VARCHAR(255) DEFAULT NULL,
+  `encounter_reference` VARCHAR(255) DEFAULT NULL,
+  `occurrencedatetime` VARCHAR(255) DEFAULT NULL,
+  `primarysource` BOOLEAN DEFAULT NULL,
+  `location_reference` VARCHAR(255) DEFAULT NULL,
+  `location_display` VARCHAR(255) DEFAULT NULL,
+  `patient_reference_uuid` VARCHAR(255) DEFAULT NULL,
+  `created_at` VARCHAR(255) DEFAULT NULL,
+  `updated_at` VARCHAR(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
