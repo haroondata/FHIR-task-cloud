b78284597fba757a33cb73d77c5d5c0a5664d100
dataharoon <170242230+haroondata@users.noreply.github.com>
2025-07-02 17:27:14 +0100

    added description and corrected logger

--- src/tools/db/execute_create_table_sql.py
+++ src/tools/db/execute_create_table_sql.py
@@ -9,32 +9,58 @@ from sqlalchemy import text
 
 import logging
 
+logger = logging.getLogger("pipeline")
+
 def execute_create_table_sql_queries(mysql_connection,sql_code):
     """
     
+    Check if the sql_code is avalable raise na warning
+
+
 
     Parameters
     ----------
-    connection : TYPE
-        DESCRIPTION.
+    connection : db connection
+       This is the database connection.
+    sql_code : String
+        This is the sql strings.
 
     Returns
     -------
-    df : TYPE
-        DESCRIPTION.
+    None.
 
     """
+   
+    # Check if sql code is provided
     if not sql_code.strip():
-        logging.warning("No SQL code provided to create_table.")
+        logger.warning("No SQL code provided to create_table.")
         return
     try:
+        # Convert raw SQL string to SQLAlchemy raw string
         sql_query_select = text(sql_code)
+        # Execute sql query
         mysql_connection.execute(sql_query_select)
-        logging.info("Table created successfully.")
+        logger.info("Table created successfully.")
     except Exception as e:
-        logging.error(f"Failed to create table. SQL: {sql_code[:100]}...")
-        logging.exception(e)
+        logger.error(f"Failed to create table. SQL: {sql_code[:100]}...")
+        logger.exception(e)
     
    
    
-   
+def create_audit_log_table(mysql_connection):
+    try:
+        audit_log_create_statement = """CREATE TABLE IF NOT EXISTS pipeline_audit_log (
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    resource_type VARCHAR(255),
+                                    inserted_rows INT,
+                                    inserted_at DATETIME DEFAULT CURRENT_TIMESTAMP
+                                        );
+                                """
+        sql_query_select = text(audit_log_create_statement)
+        mysql_connection.execute(sql_query_select)
+        logger.info("Table created successfully.")
+        
+    except Exception as e:
+        logger.error(f"Failed to create table. SQL: {audit_log_create_statement[:100]}...")
+        logger.exception(e)
+    
